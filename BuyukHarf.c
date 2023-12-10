367f86fee673ca4030c901dc4b83eac1cb5d8065
Ömer Faruk Altınova <88210573+omeraltinova@users.noreply.github.com>
2023-12-10 15:44:44 +0300

    Scanf update
    Week 8 lab

--- BuyukHarf.c
+++ BuyukHarf.c
@@ -5,7 +5,7 @@ int main(void){
     char kelime[255];
     int i;
     printf("Lutfen bir kelime giriniz\n");
-    scanf("%s",&kelime);
+    scanf("%s",&kelime[0]);
     for(i=0;i<255&&kelime[i]!= '\0';i++){
         if(kelime[i]<='z'&&kelime[i]>='a'){
             kelime[i]-=32;
