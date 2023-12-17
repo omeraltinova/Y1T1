d20c046ce0cb16eb17b8e411aeafec0ff93795c4
Ömer Faruk Altınova <88210573+omeraltinova@users.noreply.github.com>
2023-12-17 22:31:44 +0300

    String first example

---
+++ 9. Hafta/String.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+int main(void){
+/*
+    char isim[25];
+    char okul[25];
+    printf("Lutfen isminizi giriniz\n");
+    scanf("%s",isim);
+    printf("Merhaba %s\n",isim);
+    printf("Hangi universitedesin? ");
+    gets(okul);
+    printf("Hmm. Demek %s de okuyorsun\n",okul);*/
+
+ /*
+    char cumle[]= "Faruk yakinda 19 olacak";
+    char dizgi1[40];
+    char dizgi2[40];
+    int i;
+    sscanf(cumle,"%s %s %d",dizgi1,dizgi2,&i);
+    printf("%s %s %d",dizgi1,dizgi2,i);
+
+*/
+    return(0);
+}
